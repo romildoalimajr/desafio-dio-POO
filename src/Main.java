350577689b2276cd701ac248397eac249f1c0d8b
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-09-06 16:15:17 -0300

    Implementado as classes mentoria e dev.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,79 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBoocamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoInscritos() {
+        return conteudoInscritos;
+    }
+
+    public void setConteudoInscritos(Set<Conteudo> conteudoInscritos) {
+        this.conteudoInscritos = conteudoInscritos;
+    }
+
+    public Set<Conteudo> getConteudoConcluidos() {
+        return conteudoConcluidos;
+    }
+
+    public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudoConcluidos = conteudoConcluidos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudoInscritos == null) ? 0 : conteudoInscritos.hashCode());
+        result = prime * result + ((conteudoConcluidos == null) ? 0 : conteudoConcluidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudoInscritos == null) {
+            if (other.conteudoInscritos != null)
+                return false;
+        } else if (!conteudoInscritos.equals(other.conteudoInscritos))
+            return false;
+        if (conteudoConcluidos == null) {
+            if (other.conteudoConcluidos != null)
+                return false;
+        } else if (!conteudoConcluidos.equals(other.conteudoConcluidos))
+            return false;
+        return true;
+    }
+
+    
+}
